a92c36a364e1c86a6c3578ada7cb1ffa23499d7c
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-08 10:01:48 -0600

    Update add.php

--- add.php
+++ add.php
@@ -4,11 +4,11 @@ include "view-header.php";
 include "add.js";
 ?>     
 
-    <p id="output">This is a paragraph</p>
+    <p id="output">Add Two Values</p>
     <input type="text" id="num1">
     <input type="text" id="num2">
     <br>
-    <button onclick="Add()>Add</button>
+    <button onclick="Add()">Add</button>
     <p id="userTextOutput"></p>
 
 <?php
