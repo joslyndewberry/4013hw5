8653d7e0f3ca94336ee710a65431e38abbc38229
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-08 10:58:34 -0600

    Update add.js

--- add.js
+++ add.js
@@ -1,5 +1,5 @@
 function Add() {
   let val1 = document.getElementById('num1').value;
   let val2 = document.getElementById('num2').value;
-  document.getElementById('userTextOutput').innerHTML = val1 + val2;
+  return val1 + val2;
 }
