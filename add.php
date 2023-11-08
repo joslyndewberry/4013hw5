5f845e6598d2ef3df52d937a7650eae85ebac98d
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-11-08 09:52:32 -0600

    Update and rename add.html to add.php

---
+++ add.php
@@ -0,0 +1,15 @@
+<?php
+$pageTitle = "Add";
+include "view-header.php";
+include "add.js";
+?>     
+
+    <p id="output">This is a paragraph</p>
+    <input type="text" id="num1">
+    <input type="text" id="num2">
+    <br>
+    <button onclick="Add()>Call Function</button>
+
+<?php
+include "view-footer.php"
+?>
